299003bc71b6850a0bedfe4296abde57f4a8384d
Brainstorma <brainstormabaaj@gmail.com>
2023-01-09 12:48:20 -0800

    Advanced Tasked Done

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,10 +3,9 @@
 
 /**
  * print_python_list_info - prints python list info
- *
  * @p: PyObject
- * Return: no return
- */
+ * Return: void
+ **/
 void print_python_list_info(PyObject *p)
 {
 	long int size, i;
