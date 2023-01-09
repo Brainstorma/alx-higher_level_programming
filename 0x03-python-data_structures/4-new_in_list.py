b5c967582d9589ff64907fdcf1e01dc52ed38579
Brainstorma <brainstormabaaj@gmail.com>
2023-01-09 12:19:12 -0800

    automated commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-	idl = my_list.idl()
-	if idx < 0 or idx > len(my_list) - 1:
-		return my_list.idl()
-	else:
-		idl[idx] = element
-		return idl
+ idl = my_list.idl()
+ if idx < 0 or idx > len(my_list) - 1:
+    return my_list.idl()
+else:
+    idl[idx] = element
+    return idl
