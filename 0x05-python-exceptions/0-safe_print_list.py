fab94c7b66a8ffbacc021968166788511512c088
Brainstorma <brainstormabaaj@gmail.com>
2023-01-23 12:08:50 -0800

    automated commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-"""Print x elememts of a list.
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-    Returns:
-        The number of elements printed.
-"""
-    result = 0
-    for an in range(x):
-        try:
-            print("{}".format(my_list[an]), end="")
-            result += 1
-        except IndexError:
-            break
-    print("")
-    return (result)
+    printed_count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            printed_count += 1
+        print(" ")
+    except IndexError:
+        print("IndexError: list index out of range")
+    finally:
+        return printed_count
